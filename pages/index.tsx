d5fa673d61c747f264b8cafb4a6c985ed6f6f230
thamtumeoden13 <detective.blackcat13@gmail.com>
2023-06-09 14:41:50 +0700

    add gradient background

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Quote Generator</title>
+        <meta name="description" content="A fun project to generate quotes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* Background */}
+      <GradientBackgroundCon >
+
+      </GradientBackgroundCon>
+    </>
+  )
+}
